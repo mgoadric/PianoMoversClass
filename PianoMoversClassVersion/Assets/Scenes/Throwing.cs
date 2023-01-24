99889640ba3598971e29ff511bf486a08137267f
Mark Goadrich <goadrich@hendrix.edu>
2023-01-24 10:59:51 -0600

    throwing the pianos

---
+++ PianoMoversClassVersion/Assets/Scenes/Throwing.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(transform.right * 135);
+        print("hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
